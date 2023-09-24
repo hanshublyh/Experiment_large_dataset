ef12df7ca3c29319659642cb1dd26979b776708d
hanshublyh <2539617403@stu.xjtu.edu.cn>
2023-09-24 19:54:05 +0800

    修改了文件访问方式

--- run_a3.sh
+++ run_a3.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
-a="device.txt"
-b="main.ipynb"
-c="run_a1.sh"
-d="run_a3.sh"
-e="5_40"
-f="readme.txt"
+a="80_200"
+b="100_300"
+c="120_400"
+d="150_500"
 exp="NoExp"
 r="r3"
 l="r1+r2"
-g=".git"
-for file in `ls`;do
-    if [ $file != $a -a $file != $b -a $file != $c -a $file != $d -a $file != $e -a $file != $f -a $file != $g ]
-    then
-        cd $file
-        for i in 1 2 3 4 5 6 7 8 9 10; do
-            cd $i
-            cd $exp
-            cd $r
-            python a1.py >> a1.txt
-            cd ..
-            cd $l 
-            python a1.py >> a1.txt
-            cd ../../../
-        done
+for file in a b c d;do
+    cd $file
+    for i in 1 2 3 4 5 6 7 8 9 10; do
+        cd $i
+        cd $exp
+        cd $r
+        python a3.py >> a3.txt
         cd ..
-    fi
+        cd $l 
+        python a3.py >> a3.txt
+        cd ../../../
+    done
+    cd ..
 done
